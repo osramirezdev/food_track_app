8574af4a4040d314db366847965441bd36d40a38
osramirezlincoln <oramirez@lincoln.com.py>
2024-11-22 12:31:41 -0300

    ticket-7: se agrega status 'ESPERANDO'

--- microservices/order/app/Enums/OrderStatusEnum.php
+++ microservices/order/app/Enums/OrderStatusEnum.php
@@ -4,6 +4,7 @@ namespace Order\Enums;
 
 enum OrderStatusEnum: string {
     case PENDIENTE = 'PENDIENTE';
+    case ESPERANDO = 'ESPERANDO';
     case PROCESANDO = 'PROCESANDO';
     case LISTO = 'LISTO';
 }
