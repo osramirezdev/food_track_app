2e346ab42951e440a255e2374c7b6fbd901af75d
osramirezdev <osramirezdev@gmail.com>
2024-11-26 18:02:11 -0300

    ticket-14: configuraciones minimas para despliegue de front

--- microservices/order/start-server.sh
+++ microservices/order/start-server.sh
@@ -8,7 +8,5 @@ set +a
 php artisan serve --host=0.0.0.0 --port=${MS_ORDER_PORT:-8000} &
 
 # ejecutamos en segundo plano el consumidor
-php artisan rabbitmq:consume-orders &
-
-php artisan reverb:serve --debug
+php artisan rabbitmq:consume-orders
 
