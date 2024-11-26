81ba9d45200e7184c7c69b0d487b3a2f086e7be6
osramirezlincoln <oramirez@lincoln.com.py>
2024-11-26 14:37:59 -0300

    ticket-13: le quitamos responsabilidades al archivo de deploy, ya que no es necesario

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,20 @@
 
 MICROSERVICES_DIR="/app/microservices"
 
+start_microservices() {
+  echo "Starting deployment of microservices..."
+  for  service in $(ls -1 "$MICROSERVICES_DIR"); do
+    if [[ -d "$MICROSERVICES_DIR/$service" && -f "$MICROSERVICES_DIR/$service/docker-compose.yml" ]]; then # verificamos que exista directorio y archivo
+      echo "Deploying $service... in $MICROSERVICES_DIR/$service/"
+      (
+        cd "$MICROSERVICES_DIR/$service" || exit
+        $DOCKER_COMPOSE --env-file .env -f "$MICROSERVICES_DIR/$service/docker-compose.yml" up --build -d
+      )
+    else
+      echo "Skipping $service: Missing docker file or incorrect structure."
+    fi
+  done 
+
 main() {
   echo "Starting microservices deployment..."
 
@@ -14,6 +28,10 @@ main() {
   done
 
   echo "Deployment process completed!"
+}
+
+start_microservices
+
 }
 
 main
